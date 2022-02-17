bd94b947e01f9e8abfa1cebe0d2ab387b4963d6c
PatMan10 <PatMan10@protonmail.com>
2022-02-17 10:26:57 +0200

    Gareth helped solve rerender issue

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,36 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Drawer, Visibility } from './utils/enums';
 
-export class DrawerMeta {
-  constructor(
-    readonly visibleDrawer: Drawer,
-    readonly overlayVisibility: Visibility
-  ) {}
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  drawerMeta: DrawerMeta = new DrawerMeta(Drawer._, Visibility.HIDDEN);
+  visibleDrawer: Drawer = Drawer._;
+  overlayVisibility: Visibility = Visibility.HIDDEN;
 
-  ngOnInit(): void {
-    console.log('init => ', this.drawerMeta);
+  ngOnInit(): void {}
+
+  openDrawer = (d: Drawer) => this._openDrawer(d);
+  _openDrawer(drawer: Drawer): void {
+    this.visibleDrawer = drawer;
+    this.overlayVisibility = Visibility.VISIBLE;
   }
 
-  openDrawer(drawer: Drawer): void {
-    console.log('------ APP COMP => openDrawer ------');
-    console.log('drawerMeta b4 => ', this.drawerMeta);
-    this.drawerMeta = new DrawerMeta(drawer, Visibility.VISIBLE);
-    console.log('drawerMeta after => ', this.drawerMeta);
-  }
-
-  closeDrawer(): void {
-    console.log('------ APP COMP => closeDrawer ------');
-    console.log('drawerMeta b4 => ', this.drawerMeta);
-    this.drawerMeta = new DrawerMeta(Drawer._, Visibility.HIDDEN);
-    console.log('drawerMeta after => ', this.drawerMeta);
+  closeDrawer = () => this._closeDrawer();
+  _closeDrawer(): void {
+    this.visibleDrawer = Drawer._;
+    this.overlayVisibility = Visibility.HIDDEN;
   }
 }
