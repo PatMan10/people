810a59c38395ce2079c08ff0b06678db0fd1f817
PatMan10 <PatMan10@protonmail.com>
2022-03-11 20:03:21 +0200

    renamed icons

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
-import {ToolbarComponent} from './components/toolbar/toolbar.component';
-import {BackdropComponent} from './components/backdrop/backdrop.component';
-import {NavDrawerComponent} from './components/nav-drawer/nav-drawer.component';
-import {PersonFormComponent} from './pages/person-form/person-form.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { BackdropComponent } from './components/backdrop/backdrop.component';
+import { NavDrawerComponent } from './components/nav-drawer/nav-drawer.component';
+import { PersonFormComponent } from './pages/person-form/person-form.component';
 import { PersonDetailComponent } from './pages/person-detail/person-detail.component';
 import { PersonListComponent } from './pages/person-list/person-list.component';
 
