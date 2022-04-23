48364c70cd399ee901639f2f775f62a071a37d13
PatMan10 <PatMan10@protonmail.com>
2022-04-23 14:02:21 +0200

    swagged out logger

--- web-ui/src/app/common/utils/logger.ts
+++ web-ui/src/app/common/utils/logger.ts
@@ -3,26 +3,36 @@ export enum LogLevel {
   WARN = 1,
   INFO = 2,
   DEBUG = 3,
+  VERBOSE = 4,
 }
 
 export class Logger {
-  level = LogLevel.DEBUG;
+  level = LogLevel.VERBOSE;
 
   error(...data: any[]) {
-    if (this.level >= LogLevel.ERROR) console.error('error:', ...data);
+    if (this.level >= LogLevel.ERROR)
+      console.log('%c error:', 'color: red;', ...data);
   }
 
   warn(...data: any[]) {
-    if (this.level >= LogLevel.WARN) console.warn('warn:', ...data);
+    if (this.level >= LogLevel.WARN)
+      console.log('%c warn:', 'color: orange;', ...data);
   }
 
   info(...data: any[]) {
-    if (this.level >= LogLevel.INFO) console.info('info:', ...data);
+    if (this.level >= LogLevel.INFO)
+      console.log('%c info:', 'color: green;', ...data);
   }
 
   debug(...data: any[]) {
-    if (this.level >= LogLevel.DEBUG) console.log('debug:', ...data);
+    if (this.level >= LogLevel.DEBUG)
+      console.log('%c debug:', 'color: blue;', ...data);
+  }
+
+  verbose(...data: any[]) {
+    if (this.level >= LogLevel.VERBOSE)
+      console.log('%c verbose:', 'color: black;', ...data);
   }
 }
-const logger = new Logger;
+const logger = new Logger();
 export default logger;
