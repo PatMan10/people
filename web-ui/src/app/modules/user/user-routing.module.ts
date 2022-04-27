6f2b40de88f8ff63268c8eb426b259ea374c6ffb
PatMan10 <PatMan10@protonmail.com>
2022-04-27 17:44:33 +0200

    FIX

--- web-ui/src/app/modules/user/user-routing.module.ts
+++ web-ui/src/app/modules/user/user-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { UiUrls } from '../shared/utils/urls';
 import { UserDetailComponent } from './components/user-detail/user-detail.component';
-import { UserFormComponent } from './components//user-form/user-form.page';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 const routes: Routes = [
   // auth
